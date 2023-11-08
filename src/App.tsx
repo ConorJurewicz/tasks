c86e3dc24db3df86f62a14c1d038314f6115c8dc
Conor <65098646+ConorJurewicz@users.noreply.github.com>
2023-11-08 10:21:37 -0500

    removed quizzers

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import { GiveAttempts } from "./form-components/GiveAttempts";
 import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 import { ChangeColor } from "./form-components/ChangeColor";
-import { Quizzer } from "./quizzer/Quizzer";
 
 function App(): JSX.Element {
     return (
@@ -24,7 +23,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Conor Jurewicz
             </header>
-            <Quizzer></Quizzer>
             <hr></hr>
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
